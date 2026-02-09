0cf80cff5bb69f9f1d32bafff9a0b25abb675e38
rishabhjha2905-crypto <rishabhjha2905@gmail.com>
2026-02-09 00:47:00 -0600

    Initial Python setup and environment verification

---
+++ hello.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+print("Hello from the nbody project")
+print("NumPy version:", np.__version__)
+print("Matplotlib version:", matplotlib.__version__)
